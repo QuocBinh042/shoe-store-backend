9558cc978b5771d06e088b44a04db1d8fa9c4177
Vinh <phamhuuvinh0901@gmail.com>
2025-03-15 00:14:31 +0700

    fix:config cors cicd v3

--- src/main/java/com/shoestore/Server/config/CorsConfig.java
+++ src/main/java/com/shoestore/Server/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.shoestore.Server.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,14 +12,17 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfig {
 
+    @Value("${frontend.origin.url}")
+    private String frontendUrl;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*"); // Cho phép tất cả các nguồn
+        configuration.addAllowedOrigin(frontendUrl);  // Chỉ cho phép localhost:3000
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
+        configuration.setAllowCredentials(true);  // Cho phép gửi thông tin xác thực
+        configuration.setMaxAge(3600L);  // Thời gian hết hạn cấu hình CORS
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
