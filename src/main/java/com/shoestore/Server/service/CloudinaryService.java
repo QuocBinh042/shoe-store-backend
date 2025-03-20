40b14d96700b0b79a7f7d42a5565e5b3d529969b
QuocBinh042 <tranlequocbinh2003@gmail.com>
2025-03-21 00:42:58 +0700

    feat(cloudinary): config cloundinary

---
+++ src/main/java/com/shoestore/Server/service/CloudinaryService.java
@@ -0,0 +1,12 @@
+package com.shoestore.Server.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+public interface CloudinaryService {
+    Map upload(MultipartFile file) throws IOException;
+    Map listImagesInFolder(String folderPath) throws IOException;
+
+}
